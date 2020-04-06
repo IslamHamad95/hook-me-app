7ba29d9294ddf230be5b79c1bae30df1a3642f59
IslamHamad <islam.elnil95@gmail.com>
2020-04-06 15:04:44 +0200

    1st commit

---
+++ build/precache-manifest.a599a602b23ba58dfcdf793092daee4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c1ba228f8c16d18361c6cb49f3c196",
+    "url": "/hookmeapp/index.html"
+  },
+  {
+    "revision": "17b9bb2f7c5000386017",
+    "url": "/hookmeapp/static/css/main.375e02b0.chunk.css"
+  },
+  {
+    "revision": "f500af1d12b48ea9445d",
+    "url": "/hookmeapp/static/js/2.f801b000.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hookmeapp/static/js/2.f801b000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17b9bb2f7c5000386017",
+    "url": "/hookmeapp/static/js/main.79e4cad4.chunk.js"
+  },
+  {
+    "revision": "9845eff7dc657943f872",
+    "url": "/hookmeapp/static/js/runtime-main.4c12446f.js"
+  },
+  {
+    "revision": "8766cb4c4302d2c42b793738f1860ae7",
+    "url": "/hookmeapp/static/media/cotton-shirt.8766cb4c.jpg"
+  },
+  {
+    "revision": "dde2ed90fea8068b944ca63fbd1d0630",
+    "url": "/hookmeapp/static/media/denim-shirt.dde2ed90.jpg"
+  },
+  {
+    "revision": "3a2584cf6fdef75c829f1f047aa7488c",
+    "url": "/hookmeapp/static/media/done-shopping.3a2584cf.png"
+  },
+  {
+    "revision": "5c4d09259e4629740b442c65b23dd95a",
+    "url": "/hookmeapp/static/media/model.5c4d0925.png"
+  },
+  {
+    "revision": "b62e0b0b50b469f96f6eb27c9030ae7a",
+    "url": "/hookmeapp/static/media/oxford-shirt.b62e0b0b.jpg"
+  },
+  {
+    "revision": "6610962495ca67cb0b9d51dcc60bfbbc",
+    "url": "/hookmeapp/static/media/patterned-shirt.66109624.jpg"
+  }
+]);
